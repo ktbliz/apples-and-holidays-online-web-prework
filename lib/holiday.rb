f3dec66aedaa69480edfe53d936a5707f393b317
Katie Blizzard <kblizzard11@gmail.com>
2019-02-10 22:10:42 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -109,6 +109,8 @@ all_supplies_in_holidays(holiday_hash)
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  
+  
 
 end
 

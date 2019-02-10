05929783c9abffe6ff7307242813fb7351d9c46d
Katie Blizzard <kblizzard11@gmail.com>
2019-02-10 22:29:17 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -110,12 +110,12 @@ def all_holidays_with_bbq(holiday_hash)
         
         end 
     end  
-    holiday_with_bbq
+    
   end
-
+holiday_with_bbq
 end
 
-all_holidays_with_bbq(holiday_hash)
+
 
 
 
